bc05fdf6529d0ca7f317964b7f1d0a11630bbe9e
JAlberss <johnalbers2018@u.northwestern.edu>
2018-06-18 16:55:05 -0500

    Looking up the ip to do things

--- src/read_rss.rs
+++ src/read_rss.rs
@@ -2,17 +2,34 @@ extern crate xml;
 
 use read_rss::xml::reader::{EventReader, XmlEvent};
 use std::io::prelude::*;
-use std::net::TcpStream;
+use std::net::lookup_host;
+use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
 
-const SEC_RSS_URL: &'static str = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom";
+const SEC_RSS_URL: &'static str = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom:80";
 
 pub fn read_rss() {
     println!("Reading");
-    let stream = TcpStream::connect(SEC_RSS_URL);
+    let mut addrs_iter = SEC_RSS_URL
+        .to_socket_addrs()
+        .expect("Could not get IP addr");
+
+    let addr: SocketAddr = addrs_iter.next().expect("asdf");
+    let mut stream = TcpStream::connect(addr);
     match stream {
         Ok(mut s) => read_xml(&mut s),
         Err(_) => panic!("No Stream Opened"),
     };
 }
 
-pub fn read_xml(stream: &mut TcpStream) {}
+pub fn read_xml(stream: &mut TcpStream) {
+    let parser = EventReader::new(stream);
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("name {}", name);
+            }
+            Ok(e) => println!("{:#?}", e),
+            _ => println!("Nothing"),
+        }
+    }
+}
