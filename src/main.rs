db55784ed0e43a3e4f68792e16eb193b3c3dbe51
JAlberss <johnalbers2018@u.northwestern.edu>
2018-06-26 19:55:05 -0500

    Reading in characters from the xml page

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate sec_scraper;
 
 use sec_scraper::read_rss::read_rss;
 
+const SEC_RSS_URL: &'static str = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom";
+
 fn main() {
     println!("Hello, world!");
-    read_rss();
+    read_rss(SEC_RSS_URL);
 }
--- src/read_rss.rs
+++ src/read_rss.rs
@@ -5,34 +5,54 @@ use read_rss::reqwest::{Response, StatusCode};
 use read_rss::xml::reader::{EventReader, XmlEvent};
 use std::io::prelude::*;
 
-const SEC_RSS_URL: &'static str = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom";
-
-pub fn read_rss() -> Result<StatusCode, reqwest::Error> {
+pub fn read_rss(website: &str) -> Result<StatusCode, reqwest::Error> {
     println!("Reading");
-    let xml = reqwest::get(SEC_RSS_URL)?.text()?;
+    let xml = reqwest::get(website)?.text()?;
     parse_xml(xml);
     Ok(reqwest::StatusCode::Ok)
 }
 
 pub fn parse_xml(xml: String) {
     let parser = EventReader::from_str(&xml);
+    let mut entries: Vec<String> = Vec::new();
+    let mut entry_tag: bool = false;
     for e in parser {
         match e {
             Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("name {}", name);
+                if name.local_name.contains("entry") {
+                    // This sucks
+                    entry_tag = true;
+                }
             }
-            Ok(e) => println!("{:#?}", e),
+            Ok(XmlEvent::Characters(c)) => {
+                if entry_tag {
+                    println!("chars {}", &c);
+                    entries.push(c);
+                }
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                if name.local_name.contains("entry") {
+                    entry_tag = false;
+                }
+            }
+
             _ => println!("Nothing"),
         }
     }
 }
 
+pub fn clean_xml(xml: String) {
+    unimplemented!();
+}
+
 #[cfg(test)]
 mod rss_tests {
     use super::*;
+
     #[test]
     fn read_rss_test() {
-        assert_eq!(read_rss().unwrap(), StatusCode::Ok);
+        assert_eq!(read_rss("https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom").unwrap(), StatusCode::Ok);
+        assert!(read_rss("asdfajc").is_err());
     }
 
 }
