6b068d9d326955dd3021a77ab7d34f94fe5b3c41
JAlberss <johnalbers2018@u.northwestern.edu>
2018-08-05 16:46:41 -0500

    Add get posts and posting accession numbers to postgres

--- src/postgres.rs
+++ src/postgres.rs
@@ -4,6 +4,7 @@ use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
+use super::models::{Post, NewPost};
 
 // TODO This should return a result
 pub fn establish_connection() -> PgConnection {
@@ -15,7 +16,32 @@ pub fn establish_connection() -> PgConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-//pub fn write_number(
+pub fn write_number(conn: &PgConnection, acc_number: usize) -> Result<(i32,String),diesel::result::Error> {
+    use super::schema::posts;
+
+    let acc_number = acc_number.to_string();
+    
+    let new_post = NewPost {
+        acc_number: &acc_number
+    };
+
+    diesel::insert_into(posts::table).values(&new_post).get_result(conn)
+
+    
+}
+
+pub fn get_posts(conn: &PgConnection){
+    use super::schema::posts::dsl::*;
+
+    let results = posts
+        .limit(5)
+        .load::<Post>(conn)
+        .expect("Error loading posts");
+
+    for post in results {
+        println!("{}", post.acc_number);
+    }
+}
 
 #[cfg(test)]
 mod postgres_tests {
@@ -27,6 +53,15 @@ mod postgres_tests {
         establish_connection();
     }
 
+    #[test]
+    fn write_test(){
+        use crate::schema::posts::dsl::*;
+        let conn = establish_connection();
+        write_number(&conn, 6);
+        let results = posts.limit(1).load::<Post>(&conn).expect("Error loading posts");
+        assert!(results.iter().any(|a| a.acc_number == "6"));
+    }    
+    
         
 }
 
