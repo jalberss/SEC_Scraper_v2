dbd2362c7b59f975207c62b847099265903360c7
JAlberss <johnalbers2018@u.northwestern.edu>
2018-08-06 19:26:57 -0500

    Added deleting functionality

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,36 +1,37 @@
 extern crate diesel;
 
-use diesel::prelude::*;
+use super::models::{NewPost, Post};
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
-use super::models::{Post, NewPost};
 
 // TODO This should return a result
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
-pub fn write_number(conn: &PgConnection, acc_number: usize) -> Result<(i32,String),diesel::result::Error> {
+pub fn write_number(
+    conn: &PgConnection,
+    acc_number: usize,
+) -> Result<(i32, String), diesel::result::Error> {
     use super::schema::posts;
 
     let acc_number = acc_number.to_string();
-    
+
     let new_post = NewPost {
-        acc_number: &acc_number
+        acc_number: &acc_number,
     };
 
-    diesel::insert_into(posts::table).values(&new_post).get_result(conn)
-
-    
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
 }
 
-pub fn get_posts(conn: &PgConnection){
+pub fn get_posts(conn: &PgConnection) {
     use super::schema::posts::dsl::*;
 
     let results = posts
@@ -43,25 +44,46 @@ pub fn get_posts(conn: &PgConnection){
     }
 }
 
+pub fn delete_all_posts(conn: &PgConnection) {
+    use super::schema::posts::dsl::*;
+
+    diesel::delete(posts)
+        .execute(conn)
+        .expect("Error deleting posts");
+}
+
 #[cfg(test)]
 mod postgres_tests {
     use super::*;
 
     #[test]
-    fn connection_test(){
+    fn connection_test() {
         // Test will panic otherwise
         establish_connection();
     }
 
     #[test]
-    fn write_test(){
+    fn write_test() {
         use crate::schema::posts::dsl::*;
         let conn = establish_connection();
         write_number(&conn, 6);
-        let results = posts.limit(1).load::<Post>(&conn).expect("Error loading posts");
+        let results = posts
+            .limit(1)
+            .load::<Post>(&conn)
+            .expect("Error loading posts");
         assert!(results.iter().any(|a| a.acc_number == "6"));
-    }    
-    
-        
-}
+    }
 
+    #[test]
+    fn delete_test() {
+        use crate::schema::posts::dsl::*;
+        let conn = establish_connection();
+        write_number(&conn, 6);
+        delete_all_posts(&conn);
+        let results = posts
+            .limit(1)
+            .load::<Post>(&conn)
+            .expect("Error Loading posts");
+        assert!(results.iter().next().is_none());
+    }
+}
