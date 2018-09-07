ea14bc0f352b5281a9ea8f4132e09a04e1ee8990
JAlberss <johnalbers2018@u.northwestern.edu>
2018-09-07 12:05:40 -0500

    Generalizing the database connection

--- src/postgres.rs
+++ src/postgres.rs
@@ -11,8 +11,12 @@ use std::env;
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    if let Ok(database_url) = env::var("DATABASE_URL") {
+        PgConnection::establish(&database_url)
+            .expect(&format!("Error connecting to {}", database_url))
+    } else {
+        PgConnection::establish("asdf.txt").expect("")
+    }
 }
 
 pub fn write_number(
