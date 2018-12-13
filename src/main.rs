5290b13926b32f38829e1b37aa1255b51b2a0570
JAlberss <johnalbers2018@u.northwestern.edu>
2018-12-13 17:16:52 -0600

    Bullocks

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ const SEC_RSS_URL: &str = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcu
 fn main() {
     let log_file: &Path = Path::new("accession_numbers.txt");
     println!("Hello, world!");
-    let mut etag: Option<&str> = None;
-    while (true) {
-        let (xml, etag) = get_rss(SEC_RSS_URL, etag).unwrap();
+    let etag: Option<&str> = None;
+    loop {
+        let (xml, _etag) = get_rss(SEC_RSS_URL, etag).unwrap();
         let entries = read_rss(&xml);
         if let Ok(entries) = entries {
             write_table(log_file, entries);
